1947baced809dc47320bd3e8c904a454763f9572
Nicky <nickyliu91@gmail.com>
2020-02-05 08:03:05 -0500

    added summon model

---
+++ api/models/summon.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const accountSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  summontype: String,
+  rarity: String,
+  character: String,
+  weapon: String,
+  summon: String,
+  eventtype: String
+});
+
+module.exports = mongoose.model('Account', accountSchema);
